ef41eec8af291517dd587fd0a29260eb1172acf3
Charles Ajjan <Ajjan.Charles@student.ccm.edu>
2026-09-11 10:50:58 -0400

    moving along

--- box_office_report.py
+++ box_office_report.py
@@ -1,2 +1,8 @@
 #Charles Ajjan, cmp131, Week 4, Lab 1, box office report, started on 09/10/2026
-
+MOVIE=input("enter movie name")
+Adult=input("enter Amount of Adult tickets")
+Child=input("enter Amount of Child tickets")
+gross=(adultticketx10)=(childx6)
+net=gbox*0,2
+Amount=gbox-netbox
+print("moviename:",movie)
