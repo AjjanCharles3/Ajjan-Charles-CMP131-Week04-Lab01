453a954e29fe75b4be6403723b270f6bec9508d5
Charles Ajjan <Ajjan.Charles@student.ccm.edu>
2026-09-13 11:01:51 -0400

    progressing

--- interest_earned.py
+++ interest_earned.py
@@ -1,2 +1,7 @@
 #Charles Ajjan, CMP131, week 4, Lab 1, interest enered, started on 09/10/2026
-
+Principal=(int(input()))
+Rate=(int(input()))
+compounding_period=(int(input()))
+Final_amount=(float)
+(Final_amount = Principal * (1 + Rate /compounding_period))
+(Interest_earned = Final_amount-Principal)
