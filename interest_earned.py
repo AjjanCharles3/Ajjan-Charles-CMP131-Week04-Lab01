9329ddbb09024e9ad1a9bf26beaf6996243f65fc
Charles Ajjan <Ajjan.Charles@student.ccm.edu>
2026-09-13 12:36:00 -0400

    problem solving

--- interest_earned.py
+++ interest_earned.py
@@ -1,7 +1,13 @@
 #Charles Ajjan, CMP131, week 4, Lab 1, interest enered, started on 09/10/2026
-Principal=(int(input()))
-Rate=(int(input()))
-compounding_period=(int(input()))
-Final_amount=(float)
-(Final_amount = Principal * (1 + Rate /compounding_period))
-(Interest_earned = Final_amount-Principal)
+Principal = float(input("Enter the principal amount: "))
+Rate = float(input("Enter the Monthly interest rate (as a percentage): "))
+compounding_period = int(input("Enter the compounding periods per months"))
+Rate_decimal = Rate / 100
+Final_amount = Principal * (1 + (Rate_decimal / compounding_period)) ** (compounding_period )
+Interest_earned = Final_amount - Principal
+print("Principal", Principal)
+print("Rate", Rate)
+print(f"{Rate}")
+print("compounding_period", compounding_period)
+print(f"\nFinal Amount: ${Final_amount:.2f}")
+print(f"Interest Earned: ${Interest_earned:.2f}")
