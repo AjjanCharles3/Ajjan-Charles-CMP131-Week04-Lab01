c6818a225f3749eada4181be1e40f42bef1ce8b8
Charles Ajjan <Ajjan.Charles@student.ccm.edu>
2026-09-15 22:25:38 -0400

    completed box office report DONE!

--- box_office_report.py
+++ box_office_report.py
@@ -1,23 +1,35 @@
-#Charles Ajjan, cmp131, Week 4, Lab 1, box office report, started on 09/10/2026
+#Charles Ajjan 
+#cmp131
+#Week 4
+#Lab 1
+#box office report
+#started on 09/10/2026
 Movie=input("Enter Movie Name   ")
 Adult=int(input("Enter Amount of Adult Tickets Sold   "))
 Child=int(input("Enter Amount of Child Tickets Sold   "))
 Gross=float(Adult*10+Child*6)
+Adult_Tix=(Adult*10)
+Child_Tix=(Child*6)
 Theater_net=(float(Gross*0.20))
-Distributor=Gross-Theater_net
-Amount=(float(Gross-Theater_net))
-Distributor=(float(Amount-Theater_net*0.80))
+Distributor=(float(Gross-Theater_net))
+
+
+
 
 
 print()
 print()
 print("TICKET INFORMATION")
-print(Movie)
+print(Movie title: (Movie))
 print (f"Adult tickets sold",Adult)
+print("Adult_Ticket Returns:",f"${Adult_Tix:.2f}")
+print()
 print(f"Child tickets sold",Child)
+print("Child_Ticket Returns:",f"${Child_Tix:.2f}")
 print()
 print("ACCOUNTS DIVISION")
 print("Gross:",f"${Gross:.2f}")
+print()
 print("Theater_net:",f"${Theater_net:.2f}")
-print("Amount:",f"${Amount:.2f}")
+print()
 print("Distibutor",f"${Distributor:.2f}")
